bc72df858bd61cb24ce0bbea0826e2c3f0f5117d
anabsena <anabsenaf@gmail.com>
2023-11-05 23:07:09 -0300

    saturn

--- minhas_pages/urls.py
+++ minhas_pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from minhas_pages.views import home, screenOne, screenTwo
+from minhas_pages.views import home, screenOne, screenTwo, screenThree
 
 urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
     path('home/pageOne/', screenOne, name='pageOne'),
     path('home/pageTwo/', screenTwo, name='pageTwo'),
+    path('home/pageThree/', screenThree, name='pageThree'),
 ]
